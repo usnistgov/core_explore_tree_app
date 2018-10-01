1d490a5378c2301decf4ea9b9462d4016938e18f
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2018-10-01 11:51:17 -0400

    chore: upgrade version beta1
    Change-Id: I62d531b2273e59669f8ec813821e065b03dad2ba

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_tree_app",
-    version="1.0.0-alpha2",
+    version="1.0.0-beta1",
     description="Explore Tree app",
     long_description=long_desc,
     author="NIST IT Lab",
